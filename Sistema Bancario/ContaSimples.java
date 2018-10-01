487ec27180f890ec78e4c5aeb474efa73bf938f7
tfmart <tomfmart@gmail.com>
2018-09-30 22:21:10 -0300

    Saque
    Agora a função saque consegue realizar saques, deduzindo do saldo da conta, caso seja possível

--- Sistema Bancario/ContaSimples.java
+++ Sistema Bancario/ContaSimples.java
@@ -1,24 +1,31 @@
-public class ContaSimples extends Conta{
-    //Parâmetros da classe
+public class Poupanca extends Conta {
+   //Parametros da classe
+    private double taxaJuros;
     private double saldo;
-    
+
     //Construtor
-    public ContaSimples(String correntista, int numeroDaConta, String senha) {
+    public Poupanca(String correntista, int numeroDaConta, String senha, double juros) {
         super(correntista, numeroDaConta, senha);
         this.saldo = 0.00;
+        taxaJuros = juros;
     }
-    
+
+    //Função GET p/ taxa de juros
+    public double getAccountInterestRate() {
+        return taxaJuros;
+    }
+
     //Declaração das funções abstratas
     void detalhesConta() {
         System.out.printf("Correntista: " + getAccountName() + "\n");
         System.out.printf("Numero da Conta: " + getAccountNumber() + "\n");
-        System.out.printf("Tipo de conta: Conta Simples\n");
+        System.out.printf("Tipo de conta: Poupanca\n");
         System.out.printf("Saldo: R$" + getAccountBalance() + "\n");
+        System.out.printf("Taxa Juros: R$" + getAccountInterestRate() + "\n");
     }
 
-
     void saque(double valor) {
-        if (valor <= getAccountBalance()) {
+        if(valor <= getAccountBalance())  {
             saldo = getAccountBalance();
             saldo = saldo - valor;
             setSaldo(saldo);
