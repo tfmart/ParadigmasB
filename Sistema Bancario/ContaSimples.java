2fc784d6398c048b67bdc7435219c0130d799234
tfmart <tomfmart@gmail.com>
2018-10-03 16:12:31 -0300

    Update ContaSimples.java

--- Sistema Bancario/ContaSimples.java
+++ Sistema Bancario/ContaSimples.java
@@ -1,36 +1,32 @@
-public class Poupanca extends Conta {
-   //Parametros da classe
-    private double taxaJuros;
+public class ContaSimples extends Conta{
+    //Parâmetros da classe
     private double saldo;
-
+    
     //Construtor
-    public Poupanca(String correntista, int numeroDaConta, String senha, double juros) {
+    public ContaSimples(String correntista, int numeroDaConta, String senha) {
         super(correntista, numeroDaConta, senha);
         this.saldo = 0.00;
-        taxaJuros = juros;
     }
-
-    //Função GET p/ taxa de juros
-    public double getAccountInterestRate() {
-        return taxaJuros;
-    }
-
+    
     //Declaração das funções abstratas
     void detalhesConta() {
         System.out.printf("Correntista: " + getAccountName() + "\n");
         System.out.printf("Numero da Conta: " + getAccountNumber() + "\n");
-        System.out.printf("Tipo de conta: Poupanca\n");
+        System.out.printf("Tipo de conta: Conta Simples\n");
         System.out.printf("Saldo: R$" + getAccountBalance() + "\n");
-        System.out.printf("Taxa Juros: R$" + getAccountInterestRate() + "\n");
     }
 
+
     void saque(double valor) {
-        if(valor <= getAccountBalance())  {
+        if (valor <= getAccountBalance()) {
             saldo = getAccountBalance();
             saldo = saldo - valor;
             setSaldo(saldo);
         } else {
             System.out.printf("Nao ha saldo o suficiente");
         }
+    }
+
+    void increaseInterestRate(double valor) {
     }
 }
